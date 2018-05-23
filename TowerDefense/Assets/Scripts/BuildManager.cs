8d7afa5107f947676638d0f1202a4dc65438d85b
ParhamTaher <parham.taher@gmail.com>
2018-05-23 15:40:11 -0400

    Turret placement, rotation, and firing

---
+++ TowerDefense/Assets/Scripts/BuildManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class BuildManager : MonoBehaviour {
+
+    public static BuildManager instance;
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            return;
+        }
+        instance = this;
+    }
+
+    public GameObject standardTurretPrefab;
+
+    private GameObject turretToBuild;
+
+    private void Start()
+    {
+        turretToBuild = standardTurretPrefab;
+    }
+
+    public GameObject GetTurretToBuild()
+    {
+        return turretToBuild;
+    }
+
+}
---
+++ TowerDefense/Assets/Scripts/Node.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class Node : MonoBehaviour {
+
+    public Color hoverColor;
+    public Vector3 positionOffset;
+
+    private Renderer rend;
+    private Color startColor;
+    private GameObject turret;
+
+    private void Start()
+    {
+        rend = GetComponent<Renderer>();
+        startColor = rend.material.color;
+    }
+
+    private void OnMouseDown()
+    {
+        if (turret != null)
+        {
+            Debug.Log("Cant build here");
+            return;
+        }
+
+        GameObject turretToBuild = BuildManager.instance.GetTurretToBuild();
+        turret = (GameObject)Instantiate(turretToBuild, transform.position + positionOffset, transform.rotation);
+    }
+
+    private void OnMouseEnter()
+    {
+       rend.material.color = hoverColor;
+    }
+
+    private void OnMouseExit()
+    {
+        rend.material.color = startColor;
+    }
+
+}
